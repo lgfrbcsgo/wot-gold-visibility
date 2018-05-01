14d420f1853f3e06255ab7fdff05c90cf2290247
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2018-05-01 20:29:36 +0200

    Fix color naming in build script

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 version=1.0.0.3
 particles_pkg=/mnt/c/Games/World_of_Tanks/res/packages/particles.pkg
-packages=( blue lightblue cyan green limegreen purple pink red orange yellow )
+packages=( blue lightblue turquoise green limegreen purple pink red orange yellow )
 colors=( 0056FF 0095FF 00FFFF 00F000 6BFF00 C000FF FF00F4 FF0000 FF9000 FFF700 )
 python2 build.py $particles_pkg core
 for ((i=0; i<${#packages[@]}; i++)); do 
