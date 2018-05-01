4d7f586ecbd502f1ecdfb23792df64b304a057ca
lgfrbcsgo <lgfrbcsgo@gmail.com>
2018-05-01 18:35:05 +0200

    Initial commit

---
+++ build.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python2.7
+
+import re
+import os
+from argparse import ArgumentParser
+from zipfile import ZipFile, ZIP_DEFLATED
+
+out_dir = './out'
+color_out_file = 'goldvisibility.color.wotmod'
+core_out_file = 'goldvisibility.core.wotmod'
+
+
+def find_dependents(zip, dependency_regex, entry_points, targets):
+    from networkx import DiGraph
+    from networkx.algorithms.simple_paths import all_simple_paths
+
+    nodes = zip.namelist()
+    dependency_graph = DiGraph()
+    dependency_graph.add_nodes_from(nodes)
+
+    for node in nodes:
+        with zip.open(node, 'r') as zipped_file:
+            content = zipped_file.read()
+            for match in dependency_regex.finditer(content):
+                dependency_graph.add_edge(node, match.group())
+
+    dependents = set()
+    actual_targets = set()
+
+    for source in entry_points:
+        for target in targets:
+            for path in all_simple_paths(dependency_graph, source, target):
+                actual_targets.add(path[-1])
+                for dependent in path[0:-1]:
+                    dependents.add(dependent)
+
+    return list(dependents), list(actual_targets)
+
+
+# https://stackoverflow.com/questions/8627835/generate-pyc-from-python-ast
+def make_bytecode(code, file_name):
+    import time
+    import marshal
+    import py_compile
+
+    code_obj = compile(code, file_name, 'exec')
+    timestamp = long(time.time())
+    bytecode = py_compile.MAGIC
+    bytecode += chr(timestamp & 0xff)
+    bytecode += chr((timestamp >> 8) & 0xff)
+    bytecode += chr((timestamp >> 16) & 0xff)
+    bytecode += chr((timestamp >> 24) & 0xff)
+    bytecode += marshal.dumps(code_obj)
+    return bytecode
+
+
+def make_prereqs_module(file_paths):
+    def resolve_file_name(file_path):
+        if file_path.endswith('.bin'):
+            return file_path[:-4] + '.xml'
+        return file_path
+
+    code = 'files=["' + '","'.join(map(resolve_file_name, file_paths)) + '"]'
+    base_name = 'scripts/client/gui/mods/goldvisibility_prereqs'
+    return base_name + '.pyc', make_bytecode(code, base_name + '.py')
+
+
+def make_main_module():
+    base_name = 'scripts/client/gui/mods/mod_goldvisibility'
+    with open('src/' + base_name + '.py', 'r') as file:
+        return base_name + '.pyc', make_bytecode(file.read(), base_name + '.py')
+
+
+def make_effects_file(zip, file_path, dependency_regex, filtered_dependencies):
+    with zip.open(file_path, 'r') as zipped_file:
+        content = zipped_file.read()
+        for match in dependency_regex.finditer(content):
+            dependency = match.group()
+            if dependency in filtered_dependencies and content[match.end(): match.end() + 5] == (b'\x00' * 5):
+                base_name, extension = dependency.rsplit('.', 1)
+                content = content[:match.start()] + base_name + b'_prem.' + extension + content[match.end() + 5:]
+        base_name, extension = file_path.rsplit('.', 1)
+        return base_name + '_prem.' + extension, content
+
+
+def make_texture(zip, file_path, color_code):
+    from wand.image import Image
+
+    red = int(color_code[:2], 16) / 255
+    green = int(color_code[2:4], 16) / 255
+    blue = int(color_code[4:], 16) / 255
+
+    base_name, extension = file_path.rsplit('.', 1)
+
+    with zip.open(file_path, 'r') as zipped_file:
+        with Image(file=zipped_file, format=extension) as img:
+            img.threshold(-1, 'red')
+            img.threshold(-1, 'green')
+            img.threshold(-1, 'blue')
+            img.evaluate('multiply', red, channel='red')
+            img.evaluate('multiply', green, channel='green')
+            img.evaluate('multiply', blue, channel='blue')
+            return base_name + '_prem.' + extension, img.make_blob()
+
+
+def make_goldvisibility_core(particles_pkg_path):
+    out_file = os.path.join(out_dir, core_out_file)
+    try:
+        os.remove(out_file)
+    except OSError:
+        pass
+
+    with ZipFile(particles_pkg_path, 'r') as zip:
+        with ZipFile(out_file, 'w') as out:
+            entry_point_regex = re.compile('particles/Shells_Eff/[a-zA-Z0-9_\-.]+\.[a-zA-Z0-9_\-.]+')
+            entry_points = [file_name for file_name in zip.namelist() if entry_point_regex.match(file_name) is not None]
+
+            textures = [
+                'particles/content_forward/PFX_textures/eff_tex.dds',
+                'particles/content_deferred/PFX_textures/eff_tex.dds'
+            ]
+
+            dependency_regex = re.compile(b'particles(\/[a-zA-Z0-9_\-.]+)+\.[a-zA-Z0-9_\-.]+')
+            dependents, targets = find_dependents(zip, dependency_regex, entry_points, textures)
+            dependencies = dependents + targets
+
+            prereqs = [
+                'particles/content_forward/PFX_textures/eff_tex_prem.dds',
+                'particles/content_deferred/PFX_textures/eff_tex_prem.dds'
+            ]
+
+            for dependent in dependents:
+                file_name, content = make_effects_file(zip, dependent, dependency_regex, dependencies)
+                prereqs.append(file_name)
+                out.writestr('res/' + file_name, content)
+
+            file_name, content = make_prereqs_module(prereqs)
+            out.writestr('res/' + file_name, content)
+
+            file_name, content = make_main_module()
+            out.writestr('res/' + file_name, content)
+
+
+def make_goldvisibility_color(particles_pkg_path, color_code):
+    out_file = os.path.join(out_dir, color_out_file)
+    try:
+        os.remove(out_file)
+    except OSError:
+        pass
+
+    with ZipFile(particles_pkg_path, 'r') as zip:
+        with ZipFile(out_file, 'w') as out:
+
+            textures = [
+                'particles/content_forward/PFX_textures/eff_tex.dds',
+                'particles/content_deferred/PFX_textures/eff_tex.dds'
+            ]
+
+            for texture in textures:
+                file_name, content = make_texture(zip, texture, color_code)
+                out.writestr('res/' + file_name, content)
+
+
+def make_package(particles_pkg_path, version, name, color_code, rebuild_core):
+    if rebuild_core or not os.path.isfile(os.path.join(out_dir, core_out_file)):
+        make_goldvisibility_core(particles_pkg_path)
+    make_goldvisibility_color(particles_pkg_path, color_code)
+
+    out_file = os.path.join(out_dir, name)
+    try:
+        os.remove(out_file)
+    except OSError:
+        pass
+
+    with ZipFile(out_file, 'w', ZIP_DEFLATED) as zip:
+        core_path = os.path.join(out_dir, core_out_file)
+        core_zip_path = os.path.join('mods', version, core_out_file)
+        zip.write(core_path, core_zip_path)
+
+        color_path = os.path.join(out_dir, color_out_file)
+        color_zip_path = os.path.join('mods', version, color_out_file)
+        zip.write(color_path, color_zip_path)
+
+        # https://stackoverflow.com/questions/18394147/recursive-sub-folder-search-and-return-files-in-a-list-python
+        def get_other_files(directory):
+            return [os.path.join(dp, f) for dp, dn, filenames in os.walk(directory) for f in filenames]
+
+        other_files = get_other_files('mods') + get_other_files('res_mods')
+        for file in other_files:
+            zip.write(file)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('particles_pkg_path')
+    subparsers = parser.add_subparsers(dest='subparser_name')
+
+    core_parser = subparsers.add_parser('core')
+
+    color_parser = subparsers.add_parser('color')
+    color_parser.add_argument('color_code')
+
+    package_parser = subparsers.add_parser('package')
+    package_parser.add_argument('wot_version')
+    package_parser.add_argument('package_name')
+    package_parser.add_argument('color_code')
+    package_parser.add_argument('-r', '--rebuild_core', action='store_true')
+
+    args = parser.parse_args()
+
+    try:
+        os.makedirs(out_dir)
+    except OSError:
+        pass
+
+    if args.subparser_name == 'core':
+        make_goldvisibility_core(args.particles_pkg_path)
+
+    elif args.subparser_name == 'color':
+        make_goldvisibility_color(args.particles_pkg_path, args.color_code)
+
+    elif args.subparser_name == 'package':
+        make_package(args.particles_pkg_path, args.wot_version, args.package_name, args.color_code, args.rebuild_core)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/scripts/client/gui/mods/mod_goldvisibility.py
@@ -0,0 +1,247 @@
+import re
+import json
+import BigWorld
+import nations
+import ResMgr
+from functools import wraps, partial
+from items import vehicles, _xml
+from constants import ITEM_DEFS_PATH
+from helpers.EffectsList import EffectsList, _PixieEffectDesc
+from helpers.bound_effects import ModelBoundEffects, StaticSceneBoundEffects
+from gui.mods.goldvisibility_prereqs import files as prerequisites
+
+
+# load and parse json file
+def load_json_from_file(file_path):
+    try:
+        with open(file_path, 'r') as json_file:
+            # credits to https://regex101.com/r/fJ1aC6/1
+            # not perfect though, "//" in strings will be matched as comments!
+            without_comments = re.sub('(\/\*[\S\s]*?\*\/)|(\/\/[^\n]*)', '', json_file.read(), flags=re.M)
+            return json.loads(without_comments)
+    except (IOError, ValueError):
+        return None
+
+
+prerequisites_holder = None
+config = load_json_from_file('res_mods/configs/goldvisibility.json') \
+         or load_json_from_file('mods/configs/goldvisibility.json') \
+         or {}
+
+
+def on_prerequisites_loaded(refs):
+    global prerequisites_holder
+    prerequisites_holder = refs
+
+
+BigWorld.loadResourceListBG(prerequisites, on_prerequisites_loaded)
+
+
+# Keep track of mutations
+class ModifiedValueManager:
+    def __init__(self):
+        self._values = []
+
+    def add_value(self, container, key, restore_value):
+        self._values.append((container, key, restore_value))
+
+    def restore(self):
+        for (container, key, restore_value) in self._values:
+            container[key] = restore_value
+        self._values = []
+
+
+# decorator for running callback before the specified function is called
+def run_before(module, func_name):
+    def decorator(callback):
+        func = getattr(module, func_name)
+
+        @wraps(func)
+        def run_before_wrapper(*args, **kwargs):
+            callback(*args, **kwargs)
+            return func(*args, **kwargs)
+
+        setattr(module, func_name, run_before_wrapper)
+        return callback
+
+    return decorator
+
+
+# Load shell prices for a nation
+def load_shell_prices(nation):
+    xml_path = ITEM_DEFS_PATH + 'vehicles/' + nation + '/components/shells.xml'
+    section = ResMgr.openSection(xml_path)
+
+    prices = {}
+    for name, subsection in section.items():
+        if name in ('icons', 'xmlns:xmlref'):
+            continue
+
+        xml_ctx = (None, xml_path + '/' + name)
+        shell_id = _xml.readInt(xml_ctx, subsection, 'id', 0, 65535)
+        prices[shell_id] = _xml.readPrice(xml_ctx, subsection, 'price')
+
+    ResMgr.purge(xml_path, True)
+    return prices
+
+
+# get particle effects for effects list
+def get_pixie_effects(effects_list):
+    effects_desc = effects_list._EffectsList__effectDescList
+    return [desc for desc in effects_desc if isinstance(desc, _PixieEffectDesc)]
+
+
+def get_gun(attacker_id, players):
+    attacker = players.get(attacker_id, None)
+
+    if attacker is None:
+        return None
+
+    gun, _ = attacker['vehicleType'].getComponentsByType('vehicleGun')
+
+    return gun
+
+
+# return gold ammo shell types for player
+def get_gold_ammo_types_from_prices(shell_prices, default_gold_ammo_types, gun):
+    if gun is None:
+        return default_gold_ammo_types
+
+    gold_ammo_types = []
+    for shot in gun.shots:
+        nation_id, shell_id = shot.shell.id
+        gold_price = shell_prices[nation_id][shell_id].get('gold', None)
+        if gold_price is not None:
+            gold_ammo_types.append(shot.shell.kind)
+
+    if len(gold_ammo_types) > 0:
+        return gold_ammo_types
+
+    # no shell buyable with gold
+    # shell is considered 'gold' if credits price is higher then the avg shell price for the gun
+    # WG removed gold price in patch 1.0.1 for gold shells
+    credits_prices = []
+    for shot in gun.shots:
+        nation_id, shell_id = shot.shell.id
+        credits_price = shell_prices[nation_id][shell_id].get('credits', 0)
+        credits_prices.append(credits_price)
+
+    avg_price = reduce(lambda total, price: total + price, credits_prices, 0) / len(credits_prices)
+    premium_shells = [index for (index, price) in enumerate(credits_prices) if price > avg_price]
+
+    return map(lambda index: gun.shots[index].shell.kind, premium_shells)
+
+
+get_gold_ammo_types = partial(
+    get_gold_ammo_types_from_prices,
+    map(lambda nation: load_shell_prices(nation), nations.NAMES),
+    config.get('gold_ammo_types_for_unspotted_vehicles', [])
+)
+
+
+# return True if the gun uses the gold shell type as standard shell type as well
+def is_gold_shell_type_ambiguous(shell_type, gun):
+    if gun is None:
+        return False
+
+    gold_shell_types = get_gold_ammo_types(gun)
+
+    if shell_type not in gold_shell_types:
+        return False
+
+    shell_types = map(lambda s: s.shell.kind, gun.shots)
+
+    occ_in_shell_types = reduce(lambda total, st: int(st == shell_type) + total, shell_types, 0)
+    occ_in_gold_shell_types = reduce(lambda total, st: int(st == shell_type) + total, gold_shell_types, 0)
+
+    return occ_in_shell_types > occ_in_gold_shell_types
+
+
+# in game id is different form the player().id
+def get_ingame_player_id(player, players):
+    player_name = player.name
+    return [player_id for (player_id, player) in players.items() if player['name'] == player_name][0]
+
+
+def is_own_player(attacker_id, player, players):
+    return get_ingame_player_id(player, players) == attacker_id
+
+
+def get_team_id(player_id, players):
+    return players.get(player_id, {}).get('team', None)
+
+
+def is_allied_player(attacker_id, player, players):
+    own_team_id = get_team_id(get_ingame_player_id(player, players), players)
+    attacker_team_id = get_team_id(attacker_id, players)
+    return attacker_team_id == own_team_id and own_team_id is not None
+
+
+def is_team_member(attacker_id, player, players):
+    return is_allied_player(attacker_id, player, players) and not is_own_player(attacker_id, player, players)
+
+
+def settings_effect_must_show(display_settings, player, players, shell_type, gun, attacker_id):
+    return (not is_gold_shell_type_ambiguous(shell_type, gun) or display_settings['display_if_ambiguous']) \
+           and (not is_own_player(attacker_id, player, players) or display_settings['display_for_own']) \
+           and (not is_team_member(attacker_id, player, players) or display_settings['display_for_allies']) \
+           and (is_allied_player(attacker_id, player, players) or display_settings['display_for_enemy'])
+
+
+effect_must_show = partial(settings_effect_must_show, {
+    'display_if_ambiguous': config.get('display_effect_if_ammo_type_ambiguous', False),
+    'display_for_own': config.get('display_effect_for_own_shots', False),
+    'display_for_allies': config.get('display_effect_for_allied_shots', False),
+    'display_for_enemy': config.get('display_effect_for_enemy_shots', True)
+})
+
+
+def shell_type_from_effect_name(effect_name):
+    shell_type = 'ARMOR_PIERCING'
+    if '_AP_CR' in effect_name.upper():
+        shell_type = 'ARMOR_PIERCING_CR'
+    elif '_HC' in effect_name.upper():
+        shell_type = 'HOLLOW_CHARGE'
+    elif '_HE' in effect_name.upper():
+        shell_type = 'HIGH_EXPLOSIVE'
+    return shell_type
+
+
+# Restore effect and modify PixieEffectDescription file names to point to modified effects for gold shells
+def restore_with_mgr_and_modify_effect(modified_file_name_mgr, effects_list, attacker_id):
+    # restore modified effects to their default
+    modified_file_name_mgr.restore()
+
+    player = BigWorld.player()
+    players = player.arena.vehicles
+
+    gun = get_gun(attacker_id, players)
+    gold_ammo_types = get_gold_ammo_types(gun)
+
+    pixie_effects = get_pixie_effects(effects_list)
+
+    for pixie_effect in pixie_effects:
+        for (index, file_path) in enumerate(pixie_effect._files):
+            match = re.search('^particles/Shells_Eff/([a-zA-Z0-9_-]+)\.xml$', file_path)
+            if match is not None:
+                effect_name = match.group(1)
+                shell_type = shell_type_from_effect_name(effect_name)
+
+                new_file_path = 'particles/Shells_Eff/' + effect_name + '_prem.xml'
+                if shell_type in gold_ammo_types and effect_must_show(player, players, shell_type, gun, attacker_id) \
+                        and prerequisites_holder is not None and prerequisites_holder.has_key(new_file_path):
+                    modified_file_name_mgr.add_value(pixie_effect._files, index, file_path)
+                    pixie_effect._files[index] = new_file_path
+
+
+restore_and_modify_effect = partial(restore_with_mgr_and_modify_effect, ModifiedValueManager())
+
+
+@run_before(StaticSceneBoundEffects, 'addNew')
+def modify_static_bound_effect(*args, **kwargs):
+    return restore_and_modify_effect(args[2], kwargs.get('attackerID', 0))
+
+
+@run_before(ModelBoundEffects, 'addNewToNode')
+def modify_model_bound_effect(*args, **kwargs):
+    return restore_and_modify_effect(args[3], kwargs.get('attackerID', 0))
