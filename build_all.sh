654dd28d9e787a4d41ae76c38c79e22937d7d916
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2018-08-29 20:48:21 +0200

    WoT 1.1.0

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-version=1.0.2.4
+version=1.1.0
 particles_pkg=/mnt/c/Games/World_of_Tanks/res/packages/particles.pkg
 packages=( blue lightblue turquoise green limegreen purple pink red orange yellow )
 colors=( 0056FF 0095FF 00FFFF 00F000 6BFF00 C000FF FF00F4 FF0000 FF9000 FFF700 )
+echo "Building core"
 python2 build.py $particles_pkg core
-for ((i=0; i<${#packages[@]}; i++)); do 
+for ((i=0; i<${#packages[@]}; i++)); do
+    echo "Building color ${packages[$i]} $((i+1))/${#packages[@]}"
 	python2 build.py $particles_pkg package $version "goldvisibility_${packages[$i]}.zip" "${colors[$i]}"
 done
